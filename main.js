f8c0f9eabfe6c005985722cc5436974afd6fef97
WFeree <ivacyka12@gmail.com>
2025-01-18 11:16:21 +0100

    Type Class modifier

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-// Scroll indicator animation
+let searchTypes = document.querySelectorAll('.search-type');
+
+searchTypes.forEach(type => {
+    type.addEventListener('click', () => {
+        searchTypes.forEach(t => t.classList.remove('selected-type'));
+        type.classList.add('selected-type');
+    });
+});
